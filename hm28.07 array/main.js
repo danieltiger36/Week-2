9d859758914424ff3276beb8de7c84158d2ea433
danieltiger36 <danieltigar36@gmail.com>
2024-07-28 23:31:06 +0300

    hm

---
+++ hm28.07 array/main.js
@@ -0,0 +1,35 @@
+console.log("hello");
+//task1
+const fruits = []
+//task2
+const numbers = [1,2,3,4,5]
+//task3
+const colors = ["red","green", "blue" ]
+//task4
+const mixed = ["red",15,true]
+//task5
+const seasons = ["spring","summer","fall",'winter']
+//task6
+const check = [1,2,3,4,5]
+console.log(check.length);
+//task7
+const emptyCheck = [] 
+    if (emptyCheck <= 0) {
+console.log("empty");
+    }
+//task8
+const dynamic = [1,2,3,4,5]
+console.log(dynamic.length);
+//task9
+dynamic.push(6);
+console.log(dynamic.length);
+//task10
+dynamic.pop();
+console.log(dynamic.length);
+//task11
+console.log(colors[0]);
+
+
+        
+
+
