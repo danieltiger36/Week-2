4916cbac0d71d06e7682ab76ef9b0470f6a0efc0
danieltiger36 <danieltigar36@gmail.com>
2024-07-29 00:01:31 +0300

    hm

---
+++ hm28.07 loops/main.js
@@ -0,0 +1,21 @@
+console.log("hello");
+//for loops exercises
+//1
+//for (let i = 10; i >= 0 ; i--) {
+//    console.log(i);
+//}
+//2
+//for (let i = 2; i >= 20; i+2) {
+//   console.log(i);
+//}
+//3
+//let sum = 0;
+//for (let i = 0; i <= 10; i++) {
+//   sum+=i;
+//  }
+ // console.log(sum);
+ //4
+ //for (let i = 0; i <=10; i++);{
+//    let by5 = i * 5;
+//console.log(by5);
+ //}
