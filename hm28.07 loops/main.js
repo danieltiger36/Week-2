de0cb760c3f9f84e503f88038433e3c9c3bf4646
danieltiger36 <danieltigar36@gmail.com>
2024-07-29 15:02:54 +0300

    loops exr with hin finish

--- hm28.07 loops/main.js
+++ hm28.07 loops/main.js
@@ -1,21 +1,71 @@
 console.log("hello");
 //for loops exercises
-//1
+//q1
 //for (let i = 10; i >= 0 ; i--) {
 //    console.log(i);
 //}
-//2
+//q2
 //for (let i = 2; i >= 20; i+2) {
 //   console.log(i);
 //}
-//3
+//q3
 //let sum = 0;
 //for (let i = 0; i <= 10; i++) {
 //   sum+=i;
 //  }
  // console.log(sum);
- //4
- //for (let i = 0; i <=10; i++);{
+ //q4
+ //for (let i = 0; i <=10; i++) {
 //    let by5 = i * 5;
 //console.log(by5);
  //}
+ //q5
+ // const numLine = 5;
+ //for (let i = 1; i <= numLine; i++) {
+ //   let line = '';
+ //   for (let j = 1; j <= i; j++) {
+ //       line += '*';
+ //     }
+ //     console.log(line);
+ //   }
+ //do.. while loop
+ //q1
+// let i = 0
+// do {
+//    console.log(i);
+//     ( i++)
+//    
+ //}
+//while (i <=5);
+//q2
+//let playAgain;
+//do { 
+//    playAgain = prompt("do you want to play again?(yes or no)").toLocaleLowerCase();
+//} while (playAgain === "yes")
+//    console.log("thanks for playing");
+//q3 not working
+//let roll;
+//do {
+ //   function rolldie ()
+ //   roll = Math.floor(Math.random() * 6)+1;
+//    console.log(`rolled ${roll}`)
+//q4
+//} while (roll !== 6);
+//let i = 1
+//do {
+//    (i*=2)
+//    console.log(i);
+//} while (i<=1000);
+//q5 
+//let names=[]
+//let inpute;
+
+//do {
+ //   inpute = prompt("enter a name(type done when finish)"); {
+ //   
+ //   if (inpute.toLowerCase() !== "done")
+//names.push(inpute);
+//}
+//} while (inpute.toLowerCase() !== "done");
+//
+//console.log("name entered", names);
