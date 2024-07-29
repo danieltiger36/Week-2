643119a3147222302fd20532d7df19dc0dd5ea9e
danieltiger36 <danieltigar36@gmail.com>
2024-07-29 19:37:48 +0300

    progres

--- 29.07.24 30 basic for loops/main.js
+++ 29.07.24 30 basic for loops/main.js
@@ -1,1 +1,38 @@
-console.log("hello");
+//q1
+//for (let i = 0; i <= 5; i++){
+//    console.log(i);
+//}
+//q2
+//for (let i = 0; i <=9; i++){
+//    console.log(i);
+//}
+//q3
+//for (let i = 10; i >= 1; i--){
+//    console.log(i);
+//}
+//q4
+//(let i = 0; i <= 10; i++) {
+//    if (i % 2 =0){
+//        console.log(i);
+//    }
+//}
+
+//q5
+//for (let i =0; i <= 10; i++){ 
+//    if (i % 2 != 0) {
+//        console.log(i);
+//    }
+//}
+//q6
+//for (let i = 0; i <= 15; i += 3){
+//    console.log(i);
+//}
+//q7
+//for (let i = 1; i <= 20; i +=2){
+//    console.log(i);
+//}
+//q8
+//for (let i = 2; i <= 20; i +=2) {
+//    console.log(i);
+//}
+//q9
